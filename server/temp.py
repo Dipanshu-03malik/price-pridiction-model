5db8cdb3bb883615e49691e787ad3e021ceea3eb
Dipanshu-03malik <160319878+Dipanshu-03malik@users.noreply.github.com>
2024-08-05 10:55:43 +0530

    ML project

---
+++ server/temp.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from flask import Flask ,request ,jsonify
+import util
+
+app=Flask(__name__)
+
+
+@app.route('/get_location_names')
+def get_location_names():
+    response = jsonify({'locations': util.get_location_names()})
+    response.headers.add('Access-Control-Allow-Origin','*')
+    return response
+
+@app.route('/predict_home_price', methods=['POST'])
+def predict_home_price():
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = int(request.form['BHK'])
+    bath = int(request.form['bath'])
+
+
+    response = jsonify({'estimated_price': util.get_estimated_price(location,total_sqft, bhk, bath)})
+    response.headers.add('Access-Control-Allow-Origin','*')
+    return response
+
+
+if __name__ == "__main__":
+    print("Python starting Flask")
+    util.load_saved_artifacts()
+    app.run()
